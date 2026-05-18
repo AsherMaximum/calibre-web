2a412c7ebf2f6f321f966b31346912f6b22d5f2f
Asher Max Schweigart <AsherMaximum@gmail.com>
2026-05-18 13:48:13 -0400

    Adding reading progress glyph to all book pages

--- cps/render_template.py
+++ cps/render_template.py
@@ -20,7 +20,7 @@ from flask import render_template, g, abort, request
 from flask_babel import gettext as _
 from werkzeug.local import LocalProxy
 from .cw_login import current_user
-from sqlalchemy.sql.expression import or_
+from sqlalchemy.sql.expression import or_, func
 
 from . import config, constants, logger, ub
 from .ub import User
@@ -116,6 +116,21 @@ def get_sidebar_config(kwargs=None):
 # Returns the template for rendering and includes the instance name
 def render_title_template(*args, **kwargs):
     sidebar, simple = get_sidebar_config(kwargs)
+    if 'reading_progress' not in kwargs and config.config_reading_progress and not current_user.is_anonymous:
+        progress_results = (
+            ub.session.query(
+                ub.KoboReadingState.book_id,
+                func.max(ub.KoboBookmark.progress_percent)
+            )
+            .join(ub.KoboBookmark, ub.KoboBookmark.kobo_reading_state_id == ub.KoboReadingState.id)
+            .filter(ub.KoboReadingState.user_id == current_user.id)
+            .filter(ub.KoboBookmark.progress_percent.isnot(None))
+            .filter(ub.KoboBookmark.progress_percent > 0)
+            .group_by(ub.KoboReadingState.book_id)
+            .having(func.max(ub.KoboBookmark.progress_percent) < 100)
+            .all()
+        )
+        kwargs['reading_progress'] = {row[0]: row[1] for row in progress_results}
     try:
         return render_template(instance=config.config_calibre_web_title, sidebar=sidebar, simple=simple,
                                accept=config.config_upload_formats.split(','),
